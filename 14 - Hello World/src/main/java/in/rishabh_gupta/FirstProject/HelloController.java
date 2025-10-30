f4c020889babe3127d34ddcd41e802f472f8ae20
Rishabh Gupta <programmer.rishabh@gmail.com>
2025-10-30 15:43:39 +0530

    Spring Boot

--- 14 - Hello World/src/main/java/in/rishabh_gupta/FirstProject/HelloController.java
+++ 14 - Hello World/src/main/java/in/rishabh_gupta/FirstProject/HelloController.java
@@ -1,13 +1,22 @@
 package in.rishabh_gupta.FirstProject;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello() throws InterruptedException {
-        return "Hello World!";
+    public HelloResponse hello() {
+        return new HelloResponse("Hello World");
+    }
+
+    @PostMapping("/hello")
+    public HelloResponse helloPost(@RequestBody String name) {
+        return new HelloResponse("Hello " + name + "!");
+    }
+
+    @GetMapping("/hello/{name}")
+    public HelloResponse helloName(@PathVariable String name) {
+        return new HelloResponse("Hello " + name + "!");
     }
 }
